d9edaa8f85c1e356fd2f9d651dc094693e3009f6
CarlosNunezMX <carlitosnu41@gmail.com>
2025-08-09 01:28:55 -0600

    [FIX]: Fix return type on planes module

--- source/modules/planes.ts
+++ source/modules/planes.ts
@@ -3,7 +3,7 @@ import type Fetch from "@common/fetch";
 import buildURL from "@common/url";
 import type Plan from "@interfaces/planes";
 
-export default async function PlanesEstudios(fetch: Fetch, session: SessionToken.Session) {
+export default async function PlanesEstudios(fetch: Fetch, session: SessionToken.Session): Promise<Plan[]> {
   const url = buildURL("https://leoalumnos-svc.udg.mx/alum/api/alumnos-esc/:code/planes-estudios", {
     code: session.userID
   });
