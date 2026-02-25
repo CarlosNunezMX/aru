af7c505c16ba7d7545d2752806c63e19a7e9591b
CarlosNunezMX <CarlosNunezMX@users.noreply.github.com>
2026-02-25 01:53:33 +0000

    LAUCHED 2.3.0

---
+++ dist/chunk-T43BHX7J.js
@@ -0,0 +1,128 @@
+import { sign } from 'jsonwebtoken';
+
+var __defProp = Object.defineProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+
+// source/auth/session.ts
+var Session = class {
+  constructor(sessionID, userID, expiration) {
+    this.sessionID = sessionID;
+    this.userID = userID;
+    this.expiration = expiration;
+  }
+  isExpirated() {
+    return this.expiration >= /* @__PURE__ */ new Date();
+  }
+};
+
+// source/auth/password.ts
+async function hashPassword(password) {
+  if (typeof Bun !== "undefined") {
+    return Bun.password.hashSync(password, "bcrypt");
+  }
+  const bcrypt = await import('bcrypt');
+  return bcrypt.hash(password, 10);
+}
+
+// source/auth/login.ts
+async function createSession(usr, pwd, fetch, hashedPassword) {
+  const url = "https://leoalumnos-svc.udg.mx/alum/api/login/validar";
+  const res = await fetch.fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      usr,
+      pwd: hashedPassword ? pwd : await hashPassword(pwd)
+    })
+  });
+  return new Session(res.id_token, res.usua_id, new Date(res.vigencia));
+}
+var createISS_default = (key) => sign({}, key, {
+  algorithm: "RS256",
+  expiresIn: 60,
+  issuer: "I0wzMC00THVNbjBzKg=="
+});
+
+// source/auth/securityTokens.ts
+var MAGIC_KEY = "b8d0343efc18fb979821e53db80b8496";
+var SEPARATOR = "~";
+var TOKEN_KEY = "yvICnGtE@IENOuAv$tIFLyABLAALnMBI~$4";
+var SecurityToken = class {
+  static retrivetoMove(transformed) {
+    const cositaPosition = transformed.indexOf(SEPARATOR);
+    return transformed.slice(cositaPosition + 1, cositaPosition + 2).charCodeAt(0);
+  }
+  // function n->9988->nd.
+  static clean(token) {
+    const separatorIndex = token.indexOf(SEPARATOR);
+    const separated = token.slice(0, separatorIndex);
+    const toMove = token.charAt(separatorIndex + 1);
+    const numb = token.charAt(separatorIndex + 2);
+    const reduce = separated.slice(numb);
+    const reduced = token.slice(0, numb);
+    return reduce + reduced + SEPARATOR + toMove;
+  }
+  // Equivale a n->9988->nc
+  static encode(sessionID, key = MAGIC_KEY) {
+    const sessionIDLenght = sessionID.length;
+    const toMove = Math.floor(15 * Math.random() + 33);
+    const magic = 122 - toMove + 1;
+    let text = "";
+    for (let i = 0; i < sessionIDLenght; i++) {
+      let acc = sessionID.charCodeAt(i) - toMove + key.charCodeAt(i) - toMove;
+      text += String.fromCharCode(acc % magic + toMove);
+    }
+    text += SEPARATOR + String.fromCharCode(toMove) + text;
+    return this.clean(text);
+  }
+  // Equivale a n->9988->dc
+  static decode(transformedSessionID, key = MAGIC_KEY) {
+    const toMoved = this.retrivetoMove(transformedSessionID);
+    const length = transformedSessionID.slice(
+      0,
+      transformedSessionID.indexOf(SEPARATOR)
+    ).length;
+    const transformToMoved = 122 - toMoved + 1;
+    let token = "";
+    for (let i = 0; i < length; i++) {
+      let acc = transformedSessionID.charCodeAt(i) - key.charCodeAt(i);
+      if (acc < 0) acc = transformToMoved + acc;
+      token += String.fromCharCode(acc + toMoved);
+    }
+    return token;
+  }
+  static getSessionToken(sessionID) {
+    const key = this.decode(this.clean(TOKEN_KEY));
+    const encodedStudentID = this.encode(sessionID, key);
+    return encodedStudentID;
+  }
+};
+
+// source/auth/index.ts
+var auth_default = SecurityToken;
+
+// source/common/httpError.ts
+var HttpError = class extends Error {
+  constructor(code, message) {
+    super(typeof message === "string" ? message : JSON.stringify(message));
+    this.code = code;
+  }
+};
+
+// source/common/url.ts
+function buildURL(url, req) {
+  const keys = Object.keys(req);
+  let newURL = url;
+  for (let key of keys)
+    newURL = newURL.replace(`:${key}`, req[key]);
+  return newURL;
+}
+/****
+ * @author CarlosNuñezMX
+ * @url ../../../docs/sessionTokens.md
+ * @license GPLv3 and Universidad de Guadalajara
+ */
+
+export { HttpError, Session, __export, auth_default, buildURL, createISS_default, createSession };
