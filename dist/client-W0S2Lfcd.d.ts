401a26e6bf59c8095298f69fa3cb9a58f7b07e0f
CarlosNunezMX <CarlosNunezMX@users.noreply.github.com>
2026-07-31 03:48:25 +0000

    LAUCHED 2.5.3

---
+++ dist/client-W0S2Lfcd.d.ts
@@ -0,0 +1,40 @@
+import { PrivateKey } from 'jsonwebtoken';
+
+declare class Fetch {
+    private key;
+    private session?;
+    private debug;
+    constructor(key: PrivateKey, session?: Session | undefined, debug?: boolean);
+    setSession(session: Session): void;
+    private buildBody;
+    fetch<T = unknown>(url: string, req?: RequestInit): Promise<T>;
+}
+
+declare class Session {
+    sessionID: string;
+    userID: string;
+    readonly expiration: Date;
+    constructor(sessionID: string, userID: string, expiration: Date);
+    isExpirated(): boolean;
+}
+
+declare class ClientCredentials {
+    readonly user: string;
+    readonly password: string;
+    readonly isPasswordHashed: boolean;
+    constructor(user: string, password: string, isPasswordHashed?: boolean);
+}
+
+declare class Client {
+    private _session?;
+    private readonly _fetch;
+    private credentials?;
+    constructor(key: PrivateKey, debug?: boolean);
+    setCredentials(credentials: ClientCredentials): Client;
+    login(credentials?: ClientCredentials): Promise<Client>;
+    set session(session: Session);
+    get session(): Session;
+    get fetch(): Fetch;
+}
+
+export { ClientCredentials as C, Fetch as F, Session as S, Client as a };
---
+++ dist/index.d.ts
@@ -0,0 +1,24 @@
+import { C as ClientCredentials, F as Fetch, S as Session, a as Client } from './client-W0S2Lfcd.js';
+import { PrivateKey } from 'jsonwebtoken';
+
+declare function createSession(credentials: ClientCredentials, fetch: Fetch): Promise<Session>;
+
+declare const _default: (key: PrivateKey) => string;
+
+declare class HttpError extends Error {
+    code: number;
+    constructor(code: number, message: string | Object);
+}
+
+declare class NotSessionError extends Error {
+    constructor();
+}
+
+type ExtractParamName<T extends string> = T extends `:${infer P}` ? P : never;
+type ExtractParams<Path extends string> = Path extends `${infer Before}/:${infer Param}/${infer After}` ? ExtractParamName<`:${Param}`> | ExtractParams<`/${After}`> : Path extends `${infer Before}/:${infer Param}` ? ExtractParamName<`:${Param}`> : never;
+type RouteParams<Path extends string> = {
+    [K in ExtractParams<Path>]: string;
+};
+declare function buildURL<Path extends string>(url: Path, req: RouteParams<Path>): Path;
+
+export { ClientCredentials, Fetch, HttpError, NotSessionError as NotSession, Session, buildURL, _default as createISS, createSession, Client as default };
